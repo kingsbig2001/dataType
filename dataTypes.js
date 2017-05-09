845e80d22a993f381fa02bc2711622a1af50d4e0
Ibeh Kingsley <kingsbig2001@yahoo.ca>
2017-05-09 20:39:46 +0100

    Modify dataTypes.js file

--- dataTypes.js
+++ dataTypes.js
@@ -1,9 +1,9 @@
-dataTypes=(argWord)=>{
+var dataTypes=(argWord)=>{
 
  	switch(typeof argWord){
 		case "string":
 		  return (argWord.length);
-		case "null", "undefined":
+		case "undefined":
 		  return "no value";
 		case "boolean":
 		  return argWord;
@@ -12,18 +12,30 @@ dataTypes=(argWord)=>{
 		    return ("less than 100");
 		  }
 		  else if (argWord > 100){
-		    return "greater than 100";
+		    return "more than 100";
 		  }
 		  else {
 		    return "equal to 100";
-		    
 		  }
 		  break;
 		case "function":
 		    return argWord(true);
 		default:
-		  return ("I am sorry");
+		  if(argWord == null){
+			return "no value"			
+		}else if(Array.isArray(argWord)){
+			if(argWord.length >= 3){
+				return argWord[2];			
+			}else{
+				return undefined;			
+			}
+		}
   }
 
 };
+
+if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )
+{
+    module.exports = dataTypes;
+}
 
